2f5b8c17bc0e55377065248501ebb496fccff3f1
Alan Beltran Pozo <alan.beltran@students.salle.url.edu>
2022-04-02 13:16:37 +0200

    HomeView Initial Look and GitIgnore Added

--- src/presentation/views/HomeView.java
+++ src/presentation/views/HomeView.java
@@ -5,8 +5,20 @@ import java.awt.*;
 
 public class HomeView extends JFrame {
 
+    private final CardLayout cardManager;
+
+
+    private JPanel mainPanel;
+
+    private JPanel sidePanel;
+
+    private JPanel playerPanel;
+
+
     public HomeView(){
         setLayout(new BorderLayout());
+        cardManager = new CardLayout();
+
         configureView();
         configureWindow();
     }
@@ -16,10 +28,23 @@ public class HomeView extends JFrame {
         setSize(1280,720);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setResizable(false);
+        setResizable(true);
     }
 
     private void configureView(){
+        sidePanel = new JPanel();
+        sidePanel.setPreferredSize(new Dimension(280,720));
+        sidePanel.setBackground(Color.black);
+        add(sidePanel,BorderLayout.WEST);
+
+        mainPanel = new JPanel(cardManager);
+        mainPanel.setBackground(Color.gray);
+        add(mainPanel,BorderLayout.CENTER);
+
+        playerPanel = new JPanel(new FlowLayout());
+        playerPanel.setBackground(Color.blue);
+        playerPanel.setPreferredSize(new Dimension(1280,80));
+        add(playerPanel,BorderLayout.SOUTH);
 
     }
 
