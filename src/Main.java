e0930702e99fb1a3afb713885f6ac4125463471f
Alvaro Alejandro Feher Argullós <alvaro.feher@students.salle.url.edu>
2022-04-08 10:57:19 +0200

    commit corrected

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,7 @@ import persistence.SQLConfigDAO;
 public class Main {
     public static void main(String[] args) {
         String [] data = SQLConfigDAO.getInstance().getData();
-        System.out.println("shdv");
-        Song song = new Song("testSong","test","test","test","","",0);
-        SQLSongDAO songD = new SQLSongDAO();
-        songD.addSong(song);
+
+
     }
 }
