1f7a2e15bf5d349f078b54f18dd274ca9228dc45
Alan Beltran Pozo <alan.beltran@students.salle.url.edu>
2022-04-21 18:12:17 +0200

    Started Implementation of Lyrics API

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 import business.BusinessFacade;
 import business.ModelFacade;
+import business.entities.Song;
 import persistence.SQL.SQLSongDAO;
 import presentation.MainView;
 import presentation.controllers.HomeController;
 import presentation.controllers.SongsController;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            SQLSongDAO sqlSongDAO = new SQLSongDAO();
+            sqlSongDAO.addSong(new Song("Highway to Hell","Highway to Hell","Rock","AC/DC","",210000,"alvarofeher"));
             BusinessFacade businessFacade = new ModelFacade();
             MainView mainView = new MainView();
             HomeController homeController = new HomeController(mainView.getHomeView(),businessFacade);
@@ -17,5 +26,6 @@ public class Main {
             mainView.registerControllers(homeController,songsController);
             mainView.start();
         });
+
     }
 }
