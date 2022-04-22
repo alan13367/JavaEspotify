12b23ffadbf3bef57e776872ffaf3f23e6f2f912
Alan Beltran Pozo <alan.beltran@students.salle.url.edu>
2022-04-22 11:10:59 +0200

    Changes in SongLyricsAPI

--- src/business/SongLyricsAPI.java
+++ src/business/SongLyricsAPI.java
@@ -6,17 +6,14 @@ import java.net.URL;
 
 public class SongLyricsAPI {
 
-    private String URL = "https://api.lyrics.ovh/v1/";
-    private String charset = "UTF-8";
-    private HttpURLConnection conn;
-    private StringBuilder result;
-    private URL urlObj;
+    private static String URL = "https://api.lyrics.ovh/v1/";
+    private static String charset = "UTF-8";
 
-    public String getLyricsJson(String artist,String title){
+    public static String getLyricsJson(String artist,String title){
         try {
-            urlObj = new URL(URL+artist+"/"+title);
+            URL urlObj = new URL(URL+artist+"/"+title);
 
-            conn = (HttpURLConnection) urlObj.openConnection();
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
             conn.setDoOutput(false);
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept-Charset", charset);
@@ -26,7 +23,7 @@ public class SongLyricsAPI {
             //Receive the response from the server
             InputStream in = new BufferedInputStream(conn.getInputStream());
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            result = new StringBuilder();
+            StringBuilder result = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
                 result.append(line);
