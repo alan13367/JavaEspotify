33abb6e83045c9c7f6ea909dd9fbb55b65e911d6
Alan Beltran Pozo <alan.beltran@students.salle.url.edu>
2022-04-22 11:03:38 +0200

    Implementation of Lyrics API and added Gson Library

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,31 @@
 import business.BusinessFacade;
 import business.ModelFacade;
+import business.SongLyricsAPI;
 import business.entities.Song;
+import com.mysql.cj.log.Log;
 import persistence.SQL.SQLSongDAO;
 import presentation.MainView;
 import presentation.controllers.HomeController;
 import presentation.controllers.SongsController;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.MediaType;
-
 import javax.swing.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            SQLSongDAO sqlSongDAO = new SQLSongDAO();
-            sqlSongDAO.addSong(new Song("Highway to Hell","Highway to Hell","Rock","AC/DC","",210000,"alvarofeher"));
             BusinessFacade businessFacade = new ModelFacade();
             MainView mainView = new MainView();
-            HomeController homeController = new HomeController(mainView.getHomeView(),businessFacade);
-            SongsController songsController = new SongsController(mainView.getHomeView().getSongsView(),businessFacade);
-            mainView.registerControllers(homeController,songsController);
+            HomeController homeController = new HomeController(mainView.getHomeView(), businessFacade);
+            SongsController songsController = new SongsController(mainView.getHomeView().getSongsView(), businessFacade);
+            mainView.registerControllers(homeController, songsController);
             mainView.start();
         });
 
+        SongLyricsAPI songLyricsAPI = new SongLyricsAPI();
+        System.out.println(songLyricsAPI.getLyricsJson("Bad+Bunny","Yonaguni"));
     }
 }
--- src/business/SongLyricsAPI.java
+++ src/business/SongLyricsAPI.java
@@ -1,3 +1,44 @@
+package business;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class SongLyricsAPI {
+
+    private String URL = "https://api.lyrics.ovh/v1/";
+    private String charset = "UTF-8";
+    private HttpURLConnection conn;
+    private StringBuilder result;
+    private URL urlObj;
+
+    public String getLyricsJson(String artist,String title){
+        try {
+            urlObj = new URL(URL+artist+"/"+title);
+
+            conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setDoOutput(false);
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept-Charset", charset);
+            conn.setConnectTimeout(15000);
+            conn.connect();
+
+            //Receive the response from the server
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            result = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+
+            return result.toString();
+
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
